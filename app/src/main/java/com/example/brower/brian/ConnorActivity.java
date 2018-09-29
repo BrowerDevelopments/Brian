c525c5133ccabba8bec2adf8136be8702e06cdf9
Connor B <cb4501@g.rit.edu>
2018-09-29 13:40:14 -0400

    New thing

---
+++ app/src/main/java/com/example/brower/brian/ConnorActivity.java
@@ -0,0 +1,14 @@
+package com.example.brower.brian;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConnorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connor);
+    }
+}
+//this is a test
