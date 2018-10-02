23b5d19bc6277c8cd0aa434356f5144d61315683
rmdevoe <rmd9481@rit.edu>
2018-10-02 15:50:44 -0400

    started adding code for buttons

--- app/src/main/java/com/example/brower/brian/RyanActivity.java
+++ app/src/main/java/com/example/brower/brian/RyanActivity.java
@@ -2,12 +2,17 @@ package com.example.brower.brian;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RyanActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ryan);
+
+        button =(Button) findViewById(R.id.button);
     }
 }
---
+++ app/src/main/java/com/example/brower/brian/RyanActivity2.java
@@ -0,0 +1,13 @@
+package com.example.brower.brian;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RyanActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ryan2);
+    }
+}
