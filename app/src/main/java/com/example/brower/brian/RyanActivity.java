35dd20e7ae691396fba1cdd882fbfb5124488871
rmdevoe <rmd9481@rit.edu>
2018-10-02 17:54:54 -0400

    added java code for buttons, running program doesn't make buttons appear

--- app/src/main/java/com/example/brower/brian/RyanActivity.java
+++ app/src/main/java/com/example/brower/brian/RyanActivity.java
@@ -1,5 +1,6 @@
 package com.example.brower.brian;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +14,18 @@ public class RyanActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ryan);
 
-        button =(Button) findViewById(R.id.button);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRyanActivity2();
+            }
+        });
+    }
+
+    public void openRyanActivity2(){
+        Intent intent = new Intent(this, RyanActivity2.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/brower/brian/RyanActivity2.java
+++ app/src/main/java/com/example/brower/brian/RyanActivity2.java
@@ -1,13 +1,30 @@
 package com.example.brower.brian;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RyanActivity2 extends AppCompatActivity {
-
+    private Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ryan2);
+
+        button2 = findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRyanActivity();
+            }
+        });
+    }
+
+    private void openRyanActivity() {
+        Intent intent = new Intent(this, RyanActivity.class);
+        startActivity(intent);
     }
 }
